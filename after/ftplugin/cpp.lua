a214569ee4fde6ff088bce2d48126fc4301c9c58
Alex Quennelle <BluePhoenixcg@gmail.com>
2024-12-02 22:04:47 -0600

    Change hilight groups for cpp

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -2,3 +2,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
+
+vim.api.nvim_set_hl(0, "@lsp.type.namespace.cpp", { link = "@operator" })
+vim.api.nvim_set_hl(0, "@lsp.typemod.class.defaultLibrary.cpp", { link = "Number" })
