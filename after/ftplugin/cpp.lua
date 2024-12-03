318cb7c2ecb32889ab7ba645901dfc2dd0423845
Alex Quennelle <bluephoenixcg@gmail.com>
2024-12-03 13:12:06 -0600

    Link cpp documentation comments to correct colour

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -5,3 +5,4 @@ vim.opt.expandtab = false
 
 vim.api.nvim_set_hl(0, "@lsp.type.namespace.cpp", { link = "@operator" })
 vim.api.nvim_set_hl(0, "@lsp.typemod.class.defaultLibrary.cpp", { link = "Number" })
+vim.api.nvim_set_hl(0, "@comment.documentation.cpp", { fg = "#274813" })
