be00c6d495809055ef8f4647fb80d8eb9f1c4e7c
Alex Quennelle <bluephoenixcg@gmail.com>
2025-02-13 15:37:36 -0600

    Fix EnsureInstalled

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,14 +10,16 @@ return {
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"omnisharp",
 					"clangd",
-					"glsl_analyzer",
-					"html",
-					"tailwindcss",
 					"eslint",
+					"glsl_analyzer",
+					"hls",
+					"html",
+					"jsonls",
+					"lua_ls",
+					"omnisharp",
+					"tailwindcss",
+					"ts_ls",
 				},
 				automatic_installation = true,
 			})
