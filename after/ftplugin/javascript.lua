68cf511ef0b3d7ec82f155ae59da064958998843
Alex Quennelle <BluePhoenixcg@gmail.com>
2025-02-24 20:08:21 -0600

    Change javascript highlight groups

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -8,3 +8,10 @@ vim.keymap.set("n", "<f5>", function()
 	vim.cmd("w")
 	vim.cmd("LiveServerStart")
 end, { buffer = true })
+
+--highlight groups--
+vim.api.nvim_set_hl(0, "@comment.documentation.javascript", { fg = "#274813" })
+vim.api.nvim_set_hl(0, "@keyword.function.javascript", { link = "@type.builtin" })
+vim.api.nvim_set_hl(0, "@keyword.javascript", { link = "@type.builtin" })
+vim.api.nvim_set_hl(0, "@function.method.call.javascript", { link = "@function.builtin" })
+vim.api.nvim_set_hl(0, "@lsp.mod.static.javascript", { link = "keyword" })
